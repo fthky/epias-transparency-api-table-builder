6a1c2b33fdc9d139536f8a6585606b60c82bda76
Fatih Kaya <50175378+fthky@users.noreply.github.com>
2022-08-18 16:09:09 +0300

    Add files via upload

---
+++ Querys.py
@@ -0,0 +1,139 @@
+import pandas as pd
+from requests import get
+import streamlit as st
+
+class Querys:
+
+    @st.cache
+    def getQuery(self, endpoint):
+        BASE_URL = "https://seffaflik.epias.com.tr/transparency/service/"
+        url = BASE_URL + endpoint
+        response_data = get(url).json()
+        return response_data
+
+    @st.cache
+    def getDate(self, endDate, startDate):
+        endpoint = f"market/day-ahead-mcp?endDate={endDate}&startDate={startDate}"
+        response_data = self.getQuery(endpoint)
+        df = pd.DataFrame.from_dict(response_data['body']['dayAheadMCPList'])
+        date = df['date']
+        return date
+
+    @st.cache
+    def realTimeConsumption(self, endDate, startDate, selected_columns):
+        if "Real Time Consumption (MWh)" in selected_columns:
+            endpoint = f"consumption/real-time-consumption?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_realTimeConsumption = pd.DataFrame.from_dict(response_data['body']['hourlyConsumptions'])
+            df_realTimeConsumption.columns = ["date", "Real Time Consumption (MWh)"]
+            return df_realTimeConsumption
+        else:
+            pass
+
+    @st.cache
+    def mcp(self, endDate, startDate, selected_columns):
+        if "MCP (TL/MWh)" in selected_columns or "MCP (EUR/MWh)" in selected_columns or "MCP (USD/MWh)" in selected_columns:  
+            endpoint = f"market/day-ahead-mcp?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_mcp = pd.DataFrame.from_dict(response_data['body']['dayAheadMCPList'])
+            df_mcp.columns = ['date', 'MCP (TL/MWh)', 'MCP (EUR/MWh)', 'MCP (USD/MWh)']
+            return df_mcp
+
+        else:
+            pass
+
+    @st.cache
+    def realTimeGeneration(self, endDate, startDate, selected_columns):
+        if 'Asphaltite Coal' in selected_columns or 'Biomass' in selected_columns or 'Black Coal' in selected_columns or 'Dammed Hydro' in selected_columns or 'Fuel Oil' in selected_columns or 'Gas Oil' in selected_columns or 'Geothermal' in selected_columns or 'Import Coal' in selected_columns or 'Import Export' in selected_columns or 'Lignite' in selected_columns or 'Lng' in selected_columns or 'Naptha' in selected_columns or 'Natural Gas' in selected_columns or 'Nucklear' in selected_columns or 'River' in selected_columns or 'Sun' in selected_columns or 'Total Generation' in selected_columns or 'Wasteheat' in selected_columns or 'Wind' in selected_columns :  
+            endpoint = f"production/real-time-generation?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_realTimeGeneration = pd.DataFrame.from_dict(response_data['body']['hourlyGenerations'])
+            df_realTimeGeneration.columns = ['Asphaltite Coal', 'Biomass', 'Black Coal', 'Dammed Hydro', 'date', 'Fuel Oil', 'Gas Oil', 'Geothermal', 'Import Coal', 'Import Export', 'Lignite', 'Lng', 'Naptha', 'Natural Gas', 'Nucklear', 'River', 'Sun', 'Total Generation', 'Wasteheat' , 'Wind']
+            return df_realTimeGeneration
+
+        else:
+            pass
+
+
+    @st.cache
+    def weightedAveragePriceIDM(self, endDate, startDate, selected_columns):
+        if "IDM - Weighted Average Price" in selected_columns:
+            endpoint = f"market/intra-day-aof?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_weightedAveragePriceIDM = pd.DataFrame.from_dict(response_data['body']['idmAofList'])
+            df_weightedAveragePriceIDM.columns = ["date", "IDM - Weighted Average Price"]
+            return df_weightedAveragePriceIDM
+        else:
+            pass
+
+#######
+
+    @st.cache
+    def primaryFrequenceCapacityPrice(self, endDate, startDate, selected_columns):
+        if "Primary Frequence Capacity Price (TL/MWh)" in selected_columns:
+            endpoint = f"market/pfc-price?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_primaryFrequenceCapacityPrice = pd.DataFrame.from_dict(response_data['body']['frequencyReservePriceList'])
+            df_primaryFrequenceCapacityPrice.columns = ["Primary Frequence Capacity Price (TL/MWh)", "date", "hour"]
+            return df_primaryFrequenceCapacityPrice
+        else:
+            pass
+
+    @st.cache
+    def primaryFrequenceCapacityAmount(self, endDate, startDate, selected_columns):
+        if "Primary Frequence Capacity Amount (MWh)" in selected_columns:
+            endpoint = f"market/pfc-amount?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_primaryFrequenceCapacityAmount = pd.DataFrame.from_dict(response_data['body']['frequencyReservePriceList'])
+            df_primaryFrequenceCapacityAmount.columns = ["hour", "date", "Primary Frequence Capacity Amount (MWh)"]
+            return df_primaryFrequenceCapacityAmount
+        else:
+            pass
+
+
+    @st.cache
+    def secondaryFrequenceCapacityPrice(self, endDate, startDate, selected_columns):
+        if "Secondary Frequence Capacity Price (TL/MWh)" in selected_columns:
+            endpoint = f"market/sfc-price?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_secondaryFrequenceCapacityPrice = pd.DataFrame.from_dict(response_data['body']['frequencyReservePriceList'])
+            df_secondaryFrequenceCapacityPrice.columns = ["Secondary Frequence Capacity Price (TL/MWh)", "date", "hour"]
+            return df_secondaryFrequenceCapacityPrice
+        else:
+            pass
+
+
+    @st.cache
+    def secondaryFrequenceCapacityAmount(self, endDate, startDate, selected_columns):
+        if "Secondary Frequence Capacity Amount (MWh)" in selected_columns:
+            endpoint = f"market/sfc-amount?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_secondaryFrequenceCapacityAmount = pd.DataFrame.from_dict(response_data['body']['frequencyReservePriceList'])
+            df_secondaryFrequenceCapacityAmount.columns = ["date", "hour", "Secondary Frequence Capacity Amount (MWh)"]
+            return df_secondaryFrequenceCapacityAmount
+        else:
+            pass
+
+    @st.cache
+    def smp(self, endDate, startDate, selected_columns):
+        if "SMP (TL/MWh)" in selected_columns or "Regulation" in selected_columns:
+            endpoint = f"market/smp?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_smp = pd.DataFrame.from_dict(response_data['body']['smpList'])
+            df_smp.columns = ["date", "SMP (TL/MWh)", "Regulation", "smpDirectionId",""]
+            return df_smp
+        else:
+            pass
+
+
+
+    @st.cache
+    def bpm(self, endDate, startDate, selected_columns):
+        if "Net Regulation (MWh)" in selected_columns or "Up Regulation 0 Coded (MWh)" in selected_columns or "Up Regulation 1 Coded (MWh)" in selected_columns or "Up Regulation 2 Coded (MWh)" in selected_columns or "Down Regulation 0 Coded (MWh)" in selected_columns or "Down Regulation 1 Coded (MWh)" in selected_columns or "Down Regulation 2 Coded (MWh)" in selected_columns or "Up Regulation Delivered (MWh)" in selected_columns or "Down Regulation Delivered (MWh)" in selected_columns or "System's Direction" in selected_columns:
+            endpoint = f"market/bpm-order-summary?endDate={endDate}&startDate={startDate}"
+            response_data = self.getQuery(endpoint)
+            df_bpm = pd.DataFrame.from_dict(response_data['body']['bpmOrderSummaryList'])
+            df_bpm.columns = ["date", "Net Regulation (MWh)", "Up Regulation 0 Coded (MWh)", "Up Regulation 1 Coded (MWh)", "Up Regulation 2 Coded (MWh)", "Down Regulation 0 Coded (MWh)","Down Regulation 1 Coded (MWh)", "Down Regulation 2 Coded (MWh)","Up Regulation Delivered (MWh)","Down Regulation Delivered (MWh)", "System's Direction", "" ]
+            return df_bpm
+        else:
+            pass
---
+++ app.py
@@ -0,0 +1,209 @@
+import datetime
+import dateutil.relativedelta
+import streamlit as st
+import pandas as pd
+from Querys import Querys
+
+query = Querys()
+
+####### Side Bar ######
+
+st.sidebar.header("Table Builder (EPIAS Transparency API)")
+st.sidebar.write("Scroll down and complete your selections. Then click the 'Create Table' button.")
+
+st.sidebar.subheader("Date")
+
+startDate = st.sidebar.date_input(
+     "Start Date",
+     datetime.date.today() + dateutil.relativedelta.relativedelta(months=-1))
+
+
+endDate = st.sidebar.date_input(
+     "End Date",
+     datetime.date.today())
+
+
+st.sidebar.markdown("***")
+
+selected_columns = []
+
+st.sidebar.subheader("Electricity Market")
+select_market_item = st.sidebar.multiselect(
+     "Please select what columns do you want. If you don't want, you can remove item.",
+     ['MCP (TL/MWh)' , 'MCP (EUR/MWh)', 'MCP (USD/MWh)', 'SMP (TL/MWh)' ,'Regulation','IDM - Weighted Average Price', "Net Regulation (MWh)", "Up Regulation 0 Coded (MWh)", "Up Regulation 1 Coded (MWh)", "Up Regulation 2 Coded (MWh)", "Down Regulation 0 Coded (MWh)","Down Regulation 1 Coded (MWh)", "Down Regulation 2 Coded (MWh)","Up Regulation Delivered (MWh)","Down Regulation Delivered (MWh)", "System's Direction",'Primary Frequence Capacity Price (TL/MWh)', 'Primary Frequence Capacity Amount (MWh)','Secondary Frequence Capacity Price (TL/MWh)', 'Secondary Frequence Capacity Amount (MWh)' ],
+     ['MCP (TL/MWh)'])
+for i in select_market_item:
+    if len(select_market_item) != 0:
+        selected_columns.append(i)
+
+st.sidebar.markdown("***")
+
+st.sidebar.subheader("Electricity Consumption")
+select_consumption = st.sidebar.multiselect(
+     "Please select what columns do you want. If you don't want, you can remove item.",
+     ['Real Time Consumption'],
+     ['Real Time Consumption'])
+for i in select_consumption:
+    if len(select_consumption) != 0:
+        selected_columns.append(i)
+
+st.sidebar.markdown("***")
+
+st.sidebar.subheader("Electricity Generation")
+select_generation = st.sidebar.multiselect(
+      "Please select what columns do you want. If you don't want, you can remove item.",
+     ['Asphaltite Coal', 'Biomass', 'Black Coal', 'Dammed Hydro', 'Fuel Oil', 'Gas Oil', 'Geothermal', 'Import Coal', 'Import Export', 'Lignite', 'Lng', 'Naptha', 'Natural Gas', 'Nucklear', 'River', 'Sun', 'Total Generation', 'Wasteheat' , 'Wind'],
+     ['Total Generation'])
+for i in select_generation:
+    if len(select_generation) != 0:
+        selected_columns.append(i)
+
+#st.write(selected_columns)
+
+st.sidebar.markdown("***")
+st.sidebar.write('Please click the button to create table')
+
+
+##### Main Page #####
+
+st.header("Table Builder (EPIAS Transparency API)")
+
+st.markdown("""
+##### Hi 👋, I'm Fatih
+- 📄 Know about my experiences [Linkedin](https://www.linkedin.com/in/fthky/)
+
+- 👨‍💻 All of my projects are available at [Github](https://github.com/fthky)
+
+- 📫 How to reach me [E-Mail](mailto:fatihkaaya@outlook.com)
+
+""")
+
+#### Create Table ####
+
+column_names = {
+    "Real Time Consumption (MWh)" : "Real Time Consumption (MWh)",
+    "MCP (TL/MWh)" : "MCP (TL/MWh)",
+    "MCP (EUR/MWh)" : "MCP (EUR/MWh)",
+    "MCP (USD/MWh)" : "MCP (USD/MWh)",
+    "Asphaltite Coal" : "Asphaltite Coal",
+    "Biomass" : "Biomass",
+    "Black Coal" : "Black Coal",
+    "Dammed Hydro" : "Dammed Hydro",
+    "Fuel Oil" : "Fuel Oil",
+    "Gas Oil" : "Gas Oil",
+    "Geothermal" : "Geothermal",
+    "Import Coal" : "Import Coal",
+    "Import Export" : "Import Export",
+    "Lignite" : "Lignite",
+    "Lng" : "Lng",
+    "Naptha" : "Naptha",
+    "Natural Gas" : "Natural Gas",
+    "Nucklear" : "Nucklear",
+    "River" : "River",
+    "Sun" : "Sun",
+    "Total Generation" : "Total Generation",
+    "Wasteheat" : "Wasteheat",
+    "Wind" : "Wind",
+    "IDM - Weighted Average Price" : "IDM - Weighted Average Price",
+    "Primary Frequence Capacity Price (TL/MWh)" : "Primary Frequence Capacity Price (TL/MWh)",
+    "Primary Frequence Capacity Amount (MWh)" : "Primary Frequence Capacity Amount (MWh)",
+    "Secondary Frequence Capacity Price (TL/MWh)" : "Secondary Frequence Capacity Price (TL/MWh)",
+    "Secondary Frequence Capacity Amount (MWh)" : "Secondary Frequence Capacity Amount (MWh)",
+    "SMP (TL/MWh)" : "SMP (TL/MWh)",
+    "Regulation" : "Regulation",
+    "Net Regulation (MWh)" : "Net Regulation (MWh)",
+    "Up Regulation 0 Coded (MWh)" : "Up Regulation 0 Coded (MWh)",
+    "Up Regulation 1 Coded (MWh)" : "Up Regulation 1 Coded (MWh)",
+    "Up Regulation 2 Coded (MWh)" : "Up Regulation 2 Coded (MWh)",
+    "Down Regulation 0 Coded (MWh)" : "Down Regulation 0 Coded (MWh)",
+    "Down Regulation 1 Coded (MWh)" : "Down Regulation 1 Coded (MWh)",
+    "Down Regulation 2 Coded (MWh)" : "Down Regulation 2 Coded (MWh)",
+    "Up Regulation Delivered (MWh)" : "Up Regulation Delivered (MWh)",
+    "Down Regulation Delivered (MWh)" : "Down Regulation Delivered (MWh)",
+    "System's Direction" : "System's Direction",
+    }
+
+names=["date"]
+
+for i in selected_columns:
+    x = column_names.get(i)
+    names.append(x)
+
+
+def create_table():
+    df_date = query.getDate(endDate,startDate)
+    df_consumption = query.realTimeConsumption(endDate,startDate,selected_columns)
+    df_mcp = query.mcp(endDate,startDate,selected_columns)
+    df_generation = query.realTimeGeneration(endDate,startDate,selected_columns)
+    df_weightedAveragePriceIDM = query.weightedAveragePriceIDM(endDate,startDate,selected_columns)
+    df_pfcp = query.primaryFrequenceCapacityPrice(endDate, startDate, selected_columns)
+    df_pfca = query.primaryFrequenceCapacityAmount(endDate, startDate, selected_columns)
+    df_sfcp = query.secondaryFrequenceCapacityPrice(endDate, startDate, selected_columns)
+    df_sfca = query.secondaryFrequenceCapacityAmount(endDate, startDate, selected_columns)
+    df_smp = query.smp(endDate, startDate, selected_columns)
+    df_bpm = query.bpm(endDate, startDate, selected_columns)
+
+    df_all = pd.concat([
+        df_date,
+        df_mcp,
+        df_consumption,
+        df_generation,
+        df_weightedAveragePriceIDM,
+        df_pfcp,
+        df_pfca,
+        df_sfcp,
+        df_sfca,
+        df_smp,
+        df_bpm,
+        ], axis=1)
+    df_merge= df_all.loc[:,~df_all.columns.duplicated()]
+
+    result = df_merge[names]
+    st.write(result)
+
+
+    @st.cache
+    def convert_df(result):
+        return result.to_csv().encode('utf-8')
+
+
+    csv = convert_df(result)
+
+    st.download_button(
+    "Download (CSV File)",
+    csv,
+    "file.csv",
+    "text/csv",
+    key='download-csv'
+    )
+
+
+##### Show Table #####
+
+if st.sidebar.button('Create Table'):
+    if len(selected_columns)==0:
+        st.markdown("""
+        #### Please choose at least one column name
+        """)
+    else:
+        st.markdown("""
+        #### Your table is ready 👇
+
+        You can download it in CSV format with the button below the table.
+        """)
+
+        create_table()
+       
+else:
+   st.markdown("""
+                ##### About Application
+                Have you ever spent a lot of time merging columns in different files to create a new dataset. 
+                I can hear you saying yes. In this application, you can create any data collection from hourly data in the 
+                'EPİAŞ Transparency API'. All you have to do is select the dates and columns you want.
+
+                ##### Built With
+
+                [EPİAS Transparency API](https://seffaflik.epias.com.tr/transparency/) | [Python](https://www.python.org) | [Streamlit](https://streamlit.io) | [Pandas](https://pandas.pydata.org)
+                """)
+
+st.sidebar.markdown("***")
